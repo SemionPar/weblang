53ba4f7e91be7e2fd004ad46aa6a062f1193a2a3
semionPar <semion.paramonow@gmail.com>
2016-10-21 22:23:41 +0200

    Manifest misunderstanding - fixed. Removed obsolete files.

--- script/patchJar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir temp
-cd temp
-7z x ../build/libs/weblang-0.0.1-SNAPSHOT.jar
-cat META-INF/MANIFEST.MF ../MANIFEST.MF > META-INF/MANIFEST.MF
-7z u ../build/libs/weblang-0.0.1-SNAPSHOT.jar *
-cd ..
-rm -rf temp
