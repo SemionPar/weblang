df210b5c581c21ae23a92dcb958eda3bbd1da9b5
semionPar <semion.paramonow@gmail.com>
2016-10-21 21:31:35 +0200

    Should run composite build with cli switch.

--- script/patchJar.sh
+++ script/patchJar.sh
@@ -3,10 +3,7 @@
 mkdir temp
 cd temp
 7z x ../build/libs/weblang-0.0.1-SNAPSHOT.jar
-cat ../MANIFEST.MF > META-INF/MANIFEST.MF
-#echo "Name: pl.weblang.Weblang" >> META-INF/MANIFEST.MF
-#echo "" >> META-INF/MANIFEST.MF
-#echo "OmegaT-Plugin: base" >> META-INF/MANIFEST.MF
+cat META-INF/MANIFEST.MF ../MANIFEST.MF > META-INF/MANIFEST.MF
 7z u ../build/libs/weblang-0.0.1-SNAPSHOT.jar *
 cd ..
 rm -rf temp
